3484a020b0fbb9a633941ea23ff593fa158fa8d7
Nischal-2056 <nischal.bh23@gmail.com>
2023-12-27 22:42:54 +0545

    m

---
+++ run.sh
@@ -0,0 +1,17 @@
+current_dir=$(pwd)
+
+cd "C:\Users\neschal\Desktop\React\Internship\newsportels"
+echo "...............activating++++++++++++virtual++++++++++++ env...................."
+. env/Scripts/activate
+
+python manage.py runserver 8001  &
+
+django_pid=$!
+
+cd "Frontend/frontend"
+
+npm run dev
+
+kill $django_pid
+
+cd "$current_dir"
